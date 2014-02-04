35a2802f23655c1c7b63acedadaf96b84e4f4e28
Abiel Reinhart <abielr@gmail.com>
2014-02-04 06:50:48 -0500

    Add Eurostat function

--- R/pdfetch.R
+++ R/pdfetch.R
@@ -4,21 +4,21 @@
 #' @param col.names an optional list of column names. If not supplied, the 
 #' @return an xts object with all the series merged together
 fetch_FRED <- function(identifiers, col.names) {
-  results <- list()
   if (missing(col.names))
     col.names <- identifiers
   else if (length(identifiers) != length(col.names))
     stop("identifiers and col.names must be of same length")
   
+  results <- list()
   for (i in 1:length(identifiers)) {
     tmp <- tempfile()
     download.file(paste0("http://research.stlouisfed.org/fred2/series/",identifiers[i],"/downloaddata/",identifiers[i],".csv"), destfile=tmp, quiet=T) 
     fr <- read.csv(tmp,na.string=".")
     unlink(tmp)
-    fr <- xts(as.matrix(fr[,2]), as.Date(fr[,1], origin="1970-01-01"))
-    dim(fr) <- c(NROW(fr),1)
-    colnames(fr) <- col.names[i]
-    results[[identifiers[i]]] <- fr
+    x <- xts(as.matrix(fr[,2]), as.Date(fr[,1], origin="1970-01-01"))
+    dim(x) <- c(NROW(x),1)
+    colnames(x) <- col.names[i]
+    results[[identifiers[i]]] <- x
   }
   
   do.call(merge.xts, results)
@@ -26,24 +26,64 @@ fetch_FRED <- function(identifiers, col.names) {
 
 #' Fetch data from European Central Bank's statistical data warehouse
 fetch_ECB <- function(identifiers, col.names) {
-  results <- list()
   if (missing(col.names))
     col.names <- identifiers
   else if (length(identifiers) != length(col.names))
     stop("identifiers and col.names must be of same length")
   
+  results <- list()
   for (i in 1:length(identifiers)) {
     tmp <- tempfile()
     download.file(paste0("http://sdw.ecb.europa.eu/quickviewexport.do?SERIES_KEY=",identifiers[i],"&type=csv"), destfile=tmp, quiet=T) 
     fr <- read.csv(tmp, header=F, stringsAsFactors=F)[-c(1:5),]
     unlink(tmp)
-    fr <- xts(as.matrix(as.numeric(fr[,2])), as.Date(fr[,1], origin="1970-01-01"))
-    dim(fr) <- c(NROW(fr),1)
-    colnames(fr) <- col.names[i]
-    results[[identifiers[i]]] <- fr
+    x <- xts(as.matrix(as.numeric(fr[,2])), as.Date(fr[,1], origin="1970-01-01"))
+    dim(x) <- c(NROW(x),1)
+    colnames(x) <- col.names[i]
+    results[[identifiers[i]]] <- x
   }
   
   do.call(merge.xts, results)
 }
 
-fetch_ECB("FM.B.U2.EUR.4F.KR.DFR.CHG")
+#' Fetch data from Eurostat
+fetch_EUROSTAT <- function(flowRef, key, startPeriod, endPeriod, col.names) {
+  if (!missing(startPeriod) && !missing(endPeriod))
+    url <- paste0("http://ec.europa.eu/eurostat/SDMX/diss-web/rest/data/",flowRef,"/",key,"/?startPeriod=",startPeriod,"&endPeriod=",endPeriod)
+  else if (!missing(startPeriod))
+    url <- paste0("http://ec.europa.eu/eurostat/SDMX/diss-web/rest/data/",flowRef,"/",key,"/?startPeriod=",startPeriod)
+  else if (!missing(endPeriod))
+    url <- paste0("http://ec.europa.eu/eurostat/SDMX/diss-web/rest/data/",flowRef,"/",key,"/?endPeriod=",endPeriod)
+  else
+    url <- paste0("http://ec.europa.eu/eurostat/SDMX/diss-web/rest/data/",flowRef,"/",key)
+  
+  tmp <- tempfile()
+  download.file(url, destfile=tmp, quiet=T, method="curl")
+  doc <- xmlInternalTreeParse(tmp)
+  unlink(tmp)
+  
+  results <- list()
+  seriesSet <- getNodeSet(doc, "//generic:Series")
+  
+  if (!missing(col.names) && length(seriesSet) != length(col.names))
+    stop(paste("col.names of wrong length: should have", length(seriesSet), "elements"))
+    
+  for (i in 1:length(seriesSet)) {
+    series <- seriesSet[[i]]
+    id <- paste0(getNodeSet(series, "generic:SeriesKey/generic:Value/@value", "generic"), collapse=".")
+    freq <- xmlGetAttr(getNodeSet(series, "generic:SeriesKey/generic:Value[@id='FREQ']", "generic")[[1]], "value")
+    dates <- as.Date(as.yearmon(unlist(getNodeSet(series, "//generic:ObsDimension/@value", "generic")), format="%Y-%m"))
+    day(dates) <- days_in_month(dates)
+    values <- as.numeric(getNodeSet(series, "//generic:ObsValue/@value", "generic"))
+    
+    x <- xts(values, dates)
+    dim(x) <- c(NROW(x),1)
+    if (!missing(col.names))
+      colnames(x) <- col.names[i]
+    else
+      colnames(x) <- id
+    results[[i]] <- x
+  }
+  
+  do.call(merge.xts, results)
+}
